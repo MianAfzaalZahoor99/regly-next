7992ca6ba8f6c53ae3ccdc9f52c023efccfd8430
MIAN AFZAAL ZAHOOR <61027154+miang786@users.noreply.github.com>
2024-05-20 23:08:36 +0500

    Update scan-face.js

--- src/sections/template/scan-face.js
+++ src/sections/template/scan-face.js
@@ -2,7 +2,7 @@ import { Box, Typography } from '@mui/material';
 import React, { useRef, useState, useEffect } from 'react';
 import Iconify from 'src/components/iconify';
 
-const CameraComponent = ({handleNext}) => {
+const CameraComponent = ({handleNext, documentPic}) => {
   const videoRef = useRef();
   const canvasRef = useRef();
   const [imageData, setImageData] = useState(null);
@@ -42,19 +42,27 @@ const CameraComponent = ({handleNext}) => {
     <Box sx={{ width: '100px', display: 'flex', flexDirection: 'column', alignItems: "center" }}>
       {/* <video
         style={{
-            // borderRadius : "1000px",
-            // width : "200px",
-            // height : "300px",
+            borderRadius : "1000px",
+            width : "200px",
+            height : "300px",
         }}
         ref={videoRef}
         autoPlay
         playsInline
-      /> */}
-      <Iconify icon="gala:portrait2" style={{ width: '150px', height: '150px', margin: '50px 0px 20px 0px'}} />
+      />
+      <track kind="captions" src="/path/to/captions.vtt" srcLang="en" label="English" />
+      </video> */}
+      {documentPic ? (
+        <Iconify icon="fluent:border-none-24-regular" style={{ color: '#FFFFFF', width: '150px', height: '150px', margin: '50px 0px 20px 0px'}} />
+      ) : (
+        <Iconify icon="gala:portrait2" style={{ width: '150px', height: '150px', margin: '50px 0px 20px 0px'}} />
+      )}
       <Box sx={{display: 'flex', alignItems: 'center', gap: '12px', width: 'auto', marginBottom: '40px'}}>
-        <Iconify icon="uiw:warning" style={{ color: 'EE416F', margin: '20px 0px' }} />
-        <Typography sx={{width: '100%', display: 'flex', alignItems: 'center', color: '#FFFFFF', fontSize: '14px', textWrap: 'nowrap'}}>Please try again</Typography>
-      </Box>
+        {!documentPic && <Iconify icon="uiw:warning" style={{ color: 'EE416F', margin: '20px 0px' }} />}
+        <Typography sx={{width: '100%', display: 'flex', alignItems: 'center', color: '#FFFFFF', fontSize: '14px', textWrap: 'nowrap'}}>
+          {documentPic ? <>Make sure the text is clearly visible</> : <>Please try again</>}
+        </Typography>
+        </Box>
       <Iconify icon="ph:radio-button-fill" style={{ color: '#EE416F', width: '100px', height: '100px'}} onClick={handleNext} />
       {/* <button onClick={handleCapture}>Take Picture</button>
       {imageData && <img src={imageData} alt="Captured" />} */}
